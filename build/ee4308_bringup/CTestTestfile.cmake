39a7ae047dd3e8198e139a4de2fc8699eed5c0af
Bryan <ashuh223@gmail.com>
2022-02-17 00:35:36 +0800

    Remove build files

--- build/ee4308_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/haikal/a446b/src/ee4308_bringup
-# Build directory: /home/haikal/a446b/build/ee4308_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
