239612955b156a186dd89e1cc0adf46596a26f3d
waffledood <itshaikalhere@hotmail.sg>
2022-02-23 18:31:37 +0800

    Remove target_alt from move.cpp

--- src/ee4308_turtle/src/move.cpp
+++ src/ee4308_turtle/src/move.cpp
@@ -115,7 +115,6 @@ int main(int argc, char **argv)
     double cmd_lin_vel_prev = 0;
     double ang_acc, constrained_ang_acc;
     double cmd_ang_vel_prev = 0;
-    Position target_alt;
     double prop;
 
     ROS_INFO(" TMOVE : ===== BEGIN =====");
@@ -136,13 +135,11 @@ int main(int argc, char **argv)
             ////////////////// MOTION CONTROLLER HERE //////////////////
             // Checking if target has been published 
             if (target.x == 0 && target.y == 0) {
-                target_alt = pos_rbt;
                 // publish speeds
                 msg_cmd.linear.x = 0;
                 msg_cmd.angular.z = 0;
                 pub_cmd.publish(msg_cmd);
             } else {
-                target_alt = target;
             
                 // Computing PID for linear velocity //
                 pos_error = dist_euc(pos_rbt, target);
@@ -202,7 +199,6 @@ int main(int argc, char **argv)
             {
                 ROS_INFO(" TMOVE : FV(%2.3f) AV(%2.3f)", cmd_lin_vel, cmd_ang_vel);
                 ROS_INFO(" pos_error: (%2.3f), ang_error: (%2.3f)", pos_error, ang_error);
-                ROS_INFO(" Target_alt: (%3.3f, %3.3f)", target_alt.x, target_alt.y);
                 ROS_INFO(" Target: (%3.3f, %3.3f)", target.x, target.y);
                 ROS_INFO(" Pose_rbt: (%3.3f, %3.3f)", pos_rbt.x, pos_rbt.y);
             }
